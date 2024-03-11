acc0fa807a7fa317c928c4b72f3a87d649f411a6
Petr Bromnik <p.bromnik@seznam.cz>
2024-03-11 11:43:31 +0100

    + all koza functions and some limitations

--- src/constants/operations.py
+++ src/constants/operations.py
@@ -2,7 +2,7 @@ import math
 import numpy as np
 
 # toodo add e^n and ln(|x|) to operations, so far they break the program
-operations = ['+', '-', '*', '%', 'sin', 'cos']
+operations = ['+', '-', '*', '%', 'sin', 'cos', 'e^n']
 op_inputs = {
 	'+': 2,
 	'-': 2,
@@ -14,13 +14,16 @@ op_inputs = {
 	'ln(|x|)': 1,
 }
 
+# max float64 value is 1.7976931348623157e+308
 op_functions = {
-	'+': lambda x, y: x + y,
-	'-': lambda x, y: x - y,
-	'*': lambda x, y: x * y,
-	'%': lambda x, y: np.mod(x, y, where=(y != 0)),
+	'+': lambda x, y: np.add(x, y, where=(x < 1e+300) & (y < 1e+300)),
+	'-': lambda x, y: np.subtract(x, y, where=(x < 1e+300) & (y < 1e+300)),
+	'*': lambda x, y: np.multiply(x, y, where=(x < 1e+300) & (y < 1e+300)),
+	'%': lambda x, y: np.mod(x, y, where=(y != 0)), # mod is not defined for y = 0
 	'sin': lambda x: np.sin(x),
 	'cos': lambda x: np.cos(x),
-	'e^n': lambda x: np.exp(x),
+	# max float64 value is 1.7976931348623157e+308, so e^x is not defined for x > 709.782712893384 (ln(1.7976931348623157e+308) = 709.782712893384)
+	# max was set to 100 to avoid overflow when calculationg fitness
+	'e^n': lambda x: np.exp(x, where=(x <= 100)),
 	'ln(|x|)': lambda x: np.log(np.absolute(x), where=(x != 0)),
 }
